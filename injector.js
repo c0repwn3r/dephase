b59e8e134d1cbdcd493f8daa1ea4ffe6ed5fbd15
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-15 10:03:24 -0400

    Update injector.js

--- injector.js
+++ injector.js
@@ -8,9 +8,28 @@ Please don't copy, learn to make this yourself.
 - pwnd by core
 */
 DEBUG_MODE = false
-function debug(msg) {
-  if DEBUG_MODE
-function init() {
+function debug(msg, thread) {
+  if (DEBUG_MODE) {
+    console.log("[" + thread + "/DEBUG] " + msg);
+  }
+}
+function info(msg, thread) {
+  console.log("[" + thread + "/INFO] " + msg);
+}
+function warn(msg, thread) {
+  console.log("[" + thread + "/WARN] " + msg);
+}
+function error(msg, thread) {
+  console.log("[" + thread + "/ERROR] " + msg);
+}
+function fatal(msg, thread) {
+  console.log("[" + thread + "/FATAL] " + msg);
+}
+function init(debug=false) {
+  DEBUG_MODE=debug;
   document.addEventListener("keydown", (e) => {
-    
+    if (e.keyCode == 192) {
+      debug("Event fired, opening GUI", "DePhase");
+    }
+  });
 }
