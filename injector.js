9dac0c24ba7ee7eb8ded9b3eba191a5f0544708a
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-15 10:13:53 -0400

    Update injector.js

--- injector.js
+++ injector.js
@@ -7,11 +7,20 @@ Please don't copy, learn to make this yourself.
 
 - pwnd by core
 */
-DEBUG_MODE = false
+d = document;
+DEBUG_MODE = false;
 function debug(msg, thread) {
   if (DEBUG_MODE) {
     console.log("[" + thread + "/DEBUG] " + msg);
   }
+}
+function enableDebugLogging() {
+  DEBUG_MODE = true;
+  debug("Enabled debug logging.", "AdvLogSrv");
+}
+function disableDebugLogging() {
+  debug("Disabled debug logging.", "AdvLogSrv");
+  DEBUG_MODE = false;
 }
 function info(msg, thread) {
   console.log("[" + thread + "/INFO] " + msg);
@@ -25,11 +34,25 @@ function error(msg, thread) {
 function fatal(msg, thread) {
   console.log("[" + thread + "/FATAL] " + msg);
 }
+function injectScriptUrl(url, id) {
+  debug("Injector util fun called for "+url, "c0repwn3r-injetor");
+  s = d.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.onload = function(){
+      debug("Injector util fun finished for"+url, "injector-async");
+  };
+  s.src = url;
+  d.getElementsByTagName('head')[0].appendChild(s);
+}
 function init(debug=false) {
-  DEBUG_MODE=debug;
-  document.addEventListener("keydown", (e) => {
+  if (debug) {enableDebugMode();} else {disableDebugMode();}
+  d.addEventListener("keydown", (e) => {
     if (e.keyCode == 192) {
       debug("Event fired, opening GUI", "DePhase");
     }
   });
 }
+function test() {
+  info("Running tests. Ignore log messages beyond this point.", "tests");
+  enableDebugMode();
