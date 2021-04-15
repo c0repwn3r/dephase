575ce69e0e1798f67579c4f6abf0e1f94919c8bb
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-15 10:37:33 -0400

    Create tinyscriptinjector.js

---
+++ tinyscriptinjector.js
@@ -0,0 +1,1 @@
+(function(d,s){script = d.createElement('script');script.type = 'text/javascript';script.async = true;script.src = s;d.getElementsByTagName('head')[0].appendChild(script);}(document));
